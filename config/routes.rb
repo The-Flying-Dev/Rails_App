5f2c504a43a4d75c37dcb4b2706228b523e332ce
The-Flying-Dev <leandranm@outlook.com>
2023-06-12 22:13:35 +0200

    Site controller, routes, views and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'site#index'
+  get 'site/about'
+  get 'site/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
